34c3bfbe453fb7074bf34425346e34b1c22e5bfa
ektashrikhande <ekta.shrikhande30@gmail.com>
2022-05-20 15:31:53 +0530

    Welcome to employee wage computation problem

--- src/EmployeeWageComputation.java
+++ src/EmployeeWageComputation.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class EmployeeWageComputation {
+public class EmployeeWageComputation {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
 }
